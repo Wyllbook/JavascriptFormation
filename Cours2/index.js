0ef58314b1cbec4d8790988076e3c1410713fb91
Wyllem <wyllbook@gmail.com>
2022-06-26 21:10:02 -0300

    fin cours 2 sans le projet

--- Cours2/index.js
+++ Cours2/index.js
@@ -135,9 +135,9 @@ form.addEventListener("submit", (e) => {
 
 // *************LOAD EVENT************
 //Load event se déclenche quand toute la page a été chargée.
-window.addEventListener("load", () => {
-  console.log("Document chargé");
-});
+// window.addEventListener("load", () => {
+//   console.log("Document chargé");
+// });
 
 // ***************FOR EACH******************
 //Foreach permet de sélectionner plusieurs éléments
@@ -148,4 +148,123 @@ boxes.forEach((box) => {
   box.addEventListener("click", (e) => {
     e.target.style.transform = "scale(0.9)"; //réduit toute les boites au clique.
   });
+});
+
+// *********************AddEventListener VS onclick*************************
+// document.body.onclick =  () => {
+//   console.log("Click !");
+// };
+//Méthode accepté mais pas recommandé car on ne peut pas propager 2 fois le meme évenement sur une balise
+//On peut l'adosser à du Html
+
+//Bubbling => déclencher un èvenement à lmla fin( de base l'eventlistener est paramétré en mode Bubling et il est de base sur false)
+// document.body.addEventListener(
+//   "click",
+//   () => {
+//     console.log("Click 1");
+//   },
+//   false
+// );
+
+// //UseCapture => se déclenche devant les autres addeventlistener
+// document.body.addEventListener(
+//   "click",
+//   () => {
+//     console.log("Click 2");
+//   },
+//   true
+// );
+
+// *******************StopPropagation******************
+// questionContainer.addEventListener("click", (e) => {
+//   // alert("Test !");
+//   e.stopPropagation();
+// });
+
+//removeEventListener
+
+// ***************BOM************
+// BOM => Browser Object Model
+// console.log(window.innerHeight); //=> savoir la hauteur de notre fenetre
+// window.open("https://web.whatsapp.com/", "cours js", "height=600, width=800");
+// // window.close()
+
+// Evenement adossé à window
+// alert("hello")
+// confirm
+btn2.addEventListener("click", () => {
+  confirm("voulez vous vraiment vous trompez ?");
+});
+
+//prompt
+btn1.addEventListener("click", () => {
+  let answer = prompt("Entrez votre nom !");
+  questionContainer.innerHTML += `<h3> Bravo ${answer}</h3>`;
+});
+
+//timer, compte à rebours
+// setTimeout(() => {
+//   questionContainer.style.borderRadius = "300px"; //logique à exécuter
+//   questionContainer.style.transition = "2.25s";
+// }, 2000); //temps en millisecondes avant de se déclencher
+
+//Pour arréter le setInterval il faut le déclarer dans une variable
+// let interval = setInterval(() => {
+//   document.body.innerHTML += `<div class='box'>
+// <h2>Nouvelle Boîte !</h2>
+// </div>`;
+// }, 1000); // va injecter la div toute les 1s au body(boucle infini)
+
+// //e.target.remove() permet de retirer un élément du DOM
+// // document.body.addEventListener("click", (e) => {
+//   console.log(e.target); //montre dans la console sur l'élément dont a cliqué
+//   clearInterval(interval);
+// }); //stop le setInterval
+
+//Location
+// console.log(location.href);
+// console.log(location.host);
+// console.log(location.pathname);
+// console.log(location.search);
+// location.replace("https://www.youtube.com/watch?v=Cpke7ZnJJvA"); //rediriger vers un autre lien
+
+// window.onload = () => {
+//   location.href = "http://twitter.fr";
+// };
+//au chargement de la page ça va nous rediriger vers Twitter
+
+//Navigator
+//console.log(navigator.userAgent); //afficher sur la console les données de navigation
+//https://developer.mozilla.org/fr/docs/web/API/Geolocation/getCurrentPosition
+
+// var options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0,
+// };
+
+// function success(pos) {
+//   var crd = pos.coords;
+
+//   console.log("Votre position actuelle est :");
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude : ${crd.longitude}`);
+//   console.log(`La précision est de ${crd.accuracy} mètres.`);
+// }
+
+// function error(err) {
+//   console.warn(`ERREUR (${err.code}): ${err.message}`);
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error, options);
+
+//History
+// console.log(history);
+// window.history.back();
+// history.go(-2) //revenir à 2 pages en arriere
+
+// ***************Set Property***********
+window.addEventListener("mousemove", (e) => {
+  nav.style.setProperty("--x", e.layerX + "px");
+  nav.style.setProperty("--y", e.layerY + "px");
 });
